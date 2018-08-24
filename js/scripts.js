7682d88198868f49fc836b63f10dad4c53abd33e
Eric Conner <theluvq@gmail.com>
2018-08-24 11:08:23 -0700

    add js logic, html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
-var sizes = ['small', 'medium', 'large']
+function Pizza() {
+  this.size = size;
+  this.toppings = toppings;
+};
+function Order() {
+  this.pizza = Pizza;
+  this.price = sumCharges;
+};
+
+
+var toppings = [["olives, 1"], ["peppers, 1"], ["mushrooms, 1"], ["daiya, 1"]];
 
 $(document).ready(function(){
-  
-
-
+  $("form.pizza-form").submit(function(event){
+    event.preventDefault();
+    var size = $("#size")
+    var toppings = $(".toppings.checkbox.checked").val();
+    console.log(sizes + toppings);
+  })
 })
+
+//front end logic
+$( ".submit-button" ).click(function() {
+  $( ".submit-button" ).change();
+});
