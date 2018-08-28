9bc973589242a22fb258ae4f6226a12b68d2b74d
Eric Conner <theluvq@gmail.com>
2018-08-27 21:32:55 -0700

    add submit form funtion

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
-function Pizza() {
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 };
-function Order() {
-  this.pizza = Pizza;
-  this.price = sumCharges;
-};
 
 
-var toppings = [["olives, 1"], ["peppers, 1"], ["mushrooms, 1"], ["daiya, 1"]];
-
-$(document).ready(function(){
-  $("form.pizza-form").submit(function(event){
-    event.preventDefault();
-    var size = $("#size")
-    var toppings = $(".toppings.checkbox.checked").val();
-    console.log(sizes + toppings);
-  })
-})
+// var sizes = ["small, 9", "medium, 13", "large, 17"]
+// var toppings = [["olives, 1"], ["peppers, 1"], ["mushrooms, 1"], ["daiya, 1"]];
 
 //front end logic
-$( ".submit-button" ).click(function() {
-  $( ".submit-button" ).change();
-});
+$(document).ready(function(){
+  // console.log(sizes + toppings);
+  $("form.pizza-form").submit(function(event){
+    event.preventDefault();
+    var size = $('input[name=opt-size]:checked').val();
+    var toppings = $('input[name=toppings]:checked').map(function(){
+      return this.value;
+    }).get();
+    console.log(size,toppings);
+  })
+})
